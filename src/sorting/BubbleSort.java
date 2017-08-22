6eaffa002ec7ddf9372920be596ce2f58050ca95
suyash.anand <suyash.anand@CSELAB02-06.bennett.edu.in>
2017-08-22 10:40:55 +0530

    bubble sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,9 +1,14 @@
 package sorting;
-
+import java.io.*;
 public class BubbleSort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("enter_the no. of elements in the array");
+		int n=Integer.parseInt(br.readLine());
+		
+	
+		
 
 	}
 
